5c9c0c1deef5e6ec9fd11271b8ba85a6ade230f9
Guilherme Modugno <modugnoguilherme@gmail.com>
2021-05-12 09:08:30 -0300

    chore: remove unused modules and change imports

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const { ApolloServer } = require('apollo-server')
-const { importSchema } = require('graphql-import')
+const { loadSchemaSync } = require('@graphql-tools/load')
+const { GraphQLFileLoader } = require('@graphql-tools/graphql-file-loader')
+const { addResolversToSchema } = require('@graphql-tools/schema')
+const { join } = require('path')
 const resolvers = require('./resolvers')
 const context = require('./context')
 
-const schemaPath = './schema/index.graphql'
+const schema = loadSchemaSync(join(__dirname, 'schema/schema.graphql'), {
+  loaders: [new GraphQLFileLoader()]
+})
 
 const server = new ApolloServer({
-  typeDefs: importSchema(schemaPath),
-  resolvers,
+  schema: addResolversToSchema({ schema, resolvers }),
   context,
   introspection: true,
   playground: true
